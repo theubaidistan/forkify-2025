bf7b90e796417ec156a196628e85355c0b5bb715
ubaidistan <ubaidistan395@gmail.com>
2025-06-06 00:32:12 +0500

    Added new Welcome Feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -181,6 +181,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 //////////////////////////////////////////////////////////////
 // 294 - Event Handlers in MVC PublisherSubscriber Pattern
 
@@ -192,7 +196,8 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  // console.log('Welcome!');
   // controlServings();
+  newFeature();
 };
 init();
