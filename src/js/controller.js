35db701744a6c35ebae1a149dabd2c8ea0d127f2
ubaidistan <ubaidistan395@gmail.com>
2025-06-06 09:19:22 +0500

    Modife

--- src/js/controller.js
+++ src/js/controller.js
@@ -181,12 +181,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-/*
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
-*/
-
 //////////////////////////////////////////////////////////////
 // 294 - Event Handlers in MVC PublisherSubscriber Pattern
 
@@ -198,8 +192,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  // console.log('Welcome!');
-  // controlServings();
-  // newFeature();
 };
 init();
